209c3d30db14631972fa9234be97d90ba6a8ee6d
Isa Goksu <info@isagoksu.com>
2010-11-21 23:53:46 -0700

    Added draw method which delegates dsl to the right extensions

--- spec/unit/spec.class-diagram.js
+++ spec/unit/spec.class-diagram.js
@@ -1,10 +1,23 @@
 describe 'ClassDiagramExtension'
+    before_each
+        class_diagram = new ClassDiagramExtension();
+    end
     describe 'tokens'
-        it 'should return list of tokens that extension parses'
-            var class_diagram = new ClassDiagramExtension();
-            
+        it 'should return list of tokens that extension parses'            
             class_diagram.tokens.length.should.be 1
             class_diagram.tokens.should.include 'class-diagram'
+        end
+    end
+    
+    describe 'is_valid'
+        it 'should return true when given token is in the token list'
+            var token = 'class-diagram'
+            class_diagram.is_valid(token).should.be true
+        end
+        
+        it 'should return false when given token is not in the token list'
+            var invalid_token = 'invalid'
+            class_diagram.is_valid(invalid_token).should.be false
         end
     end
 end
--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,11 +1,33 @@
 describe 'RDiagram'
+    before_each
+        rdiagram = new RDiagram();
+        extension = new ClassDiagramExtension();
+    end
+    
     describe 'add_extension(extension)'
         it 'should register additional diagrams'
-            var rdiagram = new RDiagram();
-            var extension = new ClassDiagramExtension();
             rdiagram.add_extension(extension);
             
+            // for (prop in this) { print("obj[" + prop + "] = " + this[prop]); };
             rdiagram.extensions.length.should.be 1
+        end
+    end
+    
+    describe 'draw(dsl)'
+        it 'should call right extension to parse the dsl'
+            var dsl = 'class-diagram { CONTENT }';
+            var expected_content = 'CONTENT';
+            
+            function FakeExtension() {
+                this.is_valid = function(token) { return true; },
+                this.draw = function(dsl) { this.content = dsl; }
+            }
+            
+            var fake_extension = new FakeExtension();
+            rdiagram.add_extension(fake_extension);
+            rdiagram.draw(dsl);
+            
+            fake_extension.content.should.be expected_content
         end
     end
 end
